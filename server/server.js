0d0b8cdd65e47fe837b2f7b1e8859c264257a0ca
BAEK <100k1@naver.com>
2022-03-22 07:08:11 +0900

    몽고 디비 연결 시키기

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-
 const app = express();
 require("dotenv").config();
+require("./config/db")();
 
 app.use(cors());
 app.use(express.json());
