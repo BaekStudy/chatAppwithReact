18c344287202f48b332f61ea4bd3955153a11ac0
BAEK <100k1@naver.com>
2022-03-22 06:57:54 +0900

    서버 작동 코드 씀

--- server/server.js
+++ server/server.js
@@ -4,3 +4,14 @@ const mongoose = require("mongoose");
 
 const app = express();
 require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("서버작동");
+});
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server Started on Port ${process.env.PORT}`);
+});
